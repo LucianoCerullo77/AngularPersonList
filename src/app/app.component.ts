eadf964f6be4d1bfdf47645055b5a79e8286d22f
Luciano Cerullo <lucianonicolas10@gmail.com>
2023-03-11 08:59:10 -0300

    ngfor implemented, with person and persons array maded and showed in app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { Persons } from './person.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Person List';
+  person: Persons[] = [
+    new Persons('Juan', 'Perez'),
+    new Persons('Laura', 'Perez'),
+    new Persons('Karla', 'Perez'),
+  ];
 }
