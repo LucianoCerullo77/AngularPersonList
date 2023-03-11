4416f716445c26a8aa42aea45f30cb09c3c009f7
Luciano Cerullo <lucianonicolas10@gmail.com>
2023-03-11 08:51:19 -0300

    person model with constructor simplified and two way binding imported in component.ts

---
+++ src/app/person.model.ts
@@ -0,0 +1,11 @@
+export class Person {
+  // name:string = ""
+  // lastName:string = ""
+
+  // constructor(name:string,lastName:string){
+  //   this.name =
+  //   this.lastName =
+  // }
+
+  constructor(public name: string, public lastName: string) {}
+}
