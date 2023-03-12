b443ec561eadd287fa7bcc17918052a03a763dd4
Luciano Cerullo <lucianonicolas10@gmail.com>
2023-03-12 13:57:20 -0300

    property binding between components maded

--- src/app/person/person.component.ts
+++ src/app/person/person.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Persons } from '../person.model';
 
 @Component({
   selector: 'app-person',
   templateUrl: './person.component.html',
-  styleUrls: ['./person.component.css']
+  styleUrls: ['./person.component.css'],
 })
 export class PersonComponent {
-
+  @Input() person: Persons;
+  @Input() indice: number;
 }
