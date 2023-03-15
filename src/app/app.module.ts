e2dd9d5d7ec8eab98576a6f3a618d759b26db3ae
Luciano Cerullo <lucianonicolas10@gmail.com>
2023-03-15 19:15:19 -0300

    testing components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { PersonComponent } from './person/person.component';
 import { FormComponent } from './form/form.component';
+import { EjemploComponent } from './ejemplo/ejemplo.component';
 
 @NgModule({
-  declarations: [AppComponent, PersonComponent, FormComponent],
+  declarations: [AppComponent, PersonComponent, FormComponent, EjemploComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
