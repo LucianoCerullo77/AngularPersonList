fb9e1a62e3d08defe3d17f0cd8280bbf75b476cc
Luciano Cerullo <lucianonicolas10@gmail.com>
2023-03-16 12:02:35 -0300

    component deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { PersonComponent } from './person/person.component';
 import { FormComponent } from './form/form.component';
-import { EjemploComponent } from './ejemplo/ejemplo.component';
+
 
 @NgModule({
-  declarations: [AppComponent, PersonComponent, FormComponent, EjemploComponent],
+  declarations: [AppComponent, PersonComponent, FormComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
