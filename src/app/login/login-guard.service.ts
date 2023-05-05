84360e004119a0b28e038fdc08bc7b6c357fdb59
Luciano Cerullo <lucianonicolas10@gmail.com>
2023-05-05 12:19:28 -0300

    guardian updated

--- src/app/login/login-guard.service.ts
+++ src/app/login/login-guard.service.ts
@@ -7,32 +7,32 @@ import {
 import { LoginService } from './login.service';
 import { Injectable } from '@angular/core';
 
-Injectable({providedIn: 'root'})
+Injectable({ providedIn: 'root' });
 
-// export class LoginGuardian implements CanActivate {
-//   constructor(private loginService: LoginService, private router: Router) {}
-
-//   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-//     if (this.loginService.isAuthenticated()) {
-//       return true;
-//     } else {
-//       this.router.navigate(['login']);
-//       return false;
-//     }
-//   }
-// }
-
-export class LoginGuardian {
+export class LoginGuardian implements CanActivate {
   constructor(private loginService: LoginService, private router: Router) {}
 
-  canActivateLogin() {
-    if (this.loginService.getIdToken() != '') {
-      console.log('true');
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    if (this.loginService.isAuthenticated()) {
       return true;
     } else {
-      console.log('false');
       this.router.navigate(['login']);
       return false;
     }
   }
 }
+
+// export class LoginGuardian {
+//   constructor(private loginService: LoginService, private router: Router) {}
+
+//   canActivateLogin() {
+//     if (this.loginService.getIdToken() != '') {
+//       console.log('true');
+//       return true;
+//     } else {
+//       console.log('false');
+//       this.router.navigate(['login']);
+//       return false;
+//     }
+//   }
+// }
